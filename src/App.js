eaa949422f9a6a751d3fef4f61015c627bab2b38
GMarcell <42812913+GMarcell@users.noreply.github.com>
2022-07-14 15:10:44 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import PokemonThumb from './components/PokemonThumb';
 
 function App() {
+  const [allPokemon, setAllPokemon] = useState([])
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon/')
+
+  const getAllPokemons = async () => {
+    const pokemons = await fetch(loadMore)
+    const data = await pokemons.json()
+    
+    setLoadMore(data.next)
+
+    function createPokemonObject (result){
+      result.forEach(async element => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${element.name}`)
+        const pokemon = await res.json()
+        setAllPokemon(currentList => [...currentList, pokemon])
+        allPokemon.sort((a, b) => a.id - b.id)
+      });
+    }
+    
+    createPokemonObject(data.results)
+  }
+
+  useEffect(() => {
+    getAllPokemons()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon</h1>
+      <div className='pokemon-container'>
+        <div className='all-container'>
+          {allPokemon.map((pokemon, index) => {
+            return(
+              <PokemonThumb id={pokemon.id} name={pokemon.name} image={pokemon.sprites.other.dream_world.front_default} type={pokemon.types[0].type.name} key={index}/>
+            )
+          })}
+        </div>
+        <button className='load-more'onClick={() => getAllPokemons()}>Load More</button>
+      </div>
     </div>
   );
 }
